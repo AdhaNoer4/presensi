356952d04c0cf79213887d45503a4630e80befbb
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2024-12-29 21:15:00 +0700

    fix : nav home

--- pegawai/layouts/header.php
+++ pegawai/layouts/header.php
@@ -19,7 +19,7 @@ $pegawai = mysqli_fetch_array($result);
     <link href="<?= base_url('assets/css/demo.min.css?1692870487') ?>" rel="stylesheet" />
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    
+
     <style>
         @import url('https://rsms.me/inter/inter.css');
 
@@ -30,7 +30,6 @@ $pegawai = mysqli_fetch_array($result);
         body {
             font-feature-settings: "cv03", "cv04", "cv11";
         }
-        
     </style>
 </head>
 
@@ -203,7 +202,7 @@ $pegawai = mysqli_fetch_array($result);
                     <div class="container-xl">
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                                <a class="nav-link" href="./">
+                                <a class="nav-link" href="<?= base_url('pegawai/home/home.php') ?>">
                                     <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
                                         <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -218,7 +217,7 @@ $pegawai = mysqli_fetch_array($result);
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="<?= base_url('pegawai/presensi/rekap_presensi.php')?>">
+                                <a class="nav-link" href="<?= base_url('pegawai/presensi/rekap_presensi.php') ?>">
                                     <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="2">
                                             <path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2"></path>
