ee28db51923709a58f188f3d7fc5a69d37a60a11
Habibie <fahrudinhabibi4@gmail.com>
2024-12-30 10:00:34 +0700

    Create Filter Rekap Presensi Bulanan and little bit maintenance

--- admin/rekap_presensi/rekap_harian.php
+++ admin/rekap_presensi/rekap_harian.php
@@ -16,13 +16,19 @@ include_once("../../config.php");
 
 
 if (empty($_GET['tanggal_dari'])) {
-    
-    $result = mysqli_query($connection, "SELECT presensi.*, pegawai.nama, pegawai.lokasi_presensi FROM presensi JOIN pegawai ON presensi.id_pegawai = pegawai.id ORDER BY tanggal_masuk DESC");
+    $tanggal_hari_ini = (date('Y-m-d'));
+    $result = mysqli_query($connection, "SELECT presensi.*, pegawai.nama, pegawai.lokasi_presensi FROM presensi JOIN pegawai ON presensi.id_pegawai = pegawai.id WHERE tanggal_masuk = '$tanggal_hari_ini' ORDER BY tanggal_masuk DESC");
 } else {
-    
+
     $tanggal_dari = $_GET['tanggal_dari'];
     $tanggal_sampai = $_GET['tanggal_sampai'];
     $result = mysqli_query($connection, "SELECT presensi.*, pegawai.nama, pegawai.lokasi_presensi FROM presensi JOIN pegawai ON presensi.id_pegawai = pegawai.id WHERE tanggal_masuk BETWEEN '$tanggal_dari' AND '$tanggal_sampai' ORDER BY tanggal_masuk DESC");
+}
+
+if (empty($_GET['tanggal_dari'])) {
+    $tanggal = date('Y-m-d');
+} else {
+    $tanggal = $_GET['tanggal_dari'] . '-' . $_GET['tanggal_sampai'];
 }
 
 ?>
@@ -47,8 +53,12 @@ if (empty($_GET['tanggal_dari'])) {
                 </form>
             </div>
         </div>
-
-        <table class="table table-bordered">
+        <?php if (empty($_GET['tanggal_dari'])) : ?>
+            <span>Rekap Presensi Tanggal: <?= date('d F Y') ?></span>
+        <?php else : ?>
+            <span>Rekap Presensi Tanggal: <?= date('d F Y', strtotime($_GET['tanggal_dari'])) . ' sampai ' . date('d F Y', strtotime($_GET['tanggal_sampai'])) ?></span>
+        <?php endif; ?>
+        <table class="table table-bordered mt-2">
             <tr class="text-center">
                 <th>No.</th>
                 <th>Nama</th>
@@ -59,74 +69,74 @@ if (empty($_GET['tanggal_dari'])) {
                 <th>Total Terlambat</th>
             </tr>
 
-<?php if(mysqli_num_rows($result) === 0){ ?>
-    <tr>
-<td colspan="6">Data rekap presensi masih kosong</td>
+            <?php if (mysqli_num_rows($result) === 0) { ?>
+                <tr>
+                    <td colspan="6">Data rekap presensi masih kosong</td>
 
-    </tr>
-    <?php } else { ?>
-    <?php $no = 1;
-    while ($rekap = mysqli_fetch_array($result)) :
-        //menghitung total jam kerja
-        $jam_tanggal_masuk = date('Y-m-d H:i:s', strtotime($rekap['tanggal_masuk'] . ' ' . $rekap['jam_masuk']));
-        $jam_tanggal_keluar = date('Y-m-d H:i:s', strtotime($rekap['tanggal_keluar'] . ' ' . $rekap['jam_keluar']));
-        $timestamp_masuk = strtotime($jam_tanggal_masuk);
-        $timestamp_keluar = strtotime($jam_tanggal_keluar);
-        $selisih = $timestamp_keluar - $timestamp_masuk;
-        $total_jam_kerja = floor($selisih / 3600);
-        $selisih -= $total_jam_kerja * 3600;
-        $selisih_menit_kerja = floor($selisih / 60);
+                </tr>
+            <?php } else { ?>
+                <?php $no = 1;
+                while ($rekap = mysqli_fetch_array($result)) :
+                    //menghitung total jam kerja
+                    $jam_tanggal_masuk = date('Y-m-d H:i:s', strtotime($rekap['tanggal_masuk'] . ' ' . $rekap['jam_masuk']));
+                    $jam_tanggal_keluar = date('Y-m-d H:i:s', strtotime($rekap['tanggal_keluar'] . ' ' . $rekap['jam_keluar']));
+                    $timestamp_masuk = strtotime($jam_tanggal_masuk);
+                    $timestamp_keluar = strtotime($jam_tanggal_keluar);
+                    $selisih = $timestamp_keluar - $timestamp_masuk;
+                    $total_jam_kerja = floor($selisih / 3600);
+                    $selisih -= $total_jam_kerja * 3600;
+                    $selisih_menit_kerja = floor($selisih / 60);
 
-        //menghitung total jam terlambat
-        $lokasi_presensi = $rekap['lokasi_presensi'];
-$lokasi = mysqli_query($connection, "SELECT * FROM lokasi_presensi WHERE nama_lokasi = '$lokasi_presensi'");
+                    //menghitung total jam terlambat
+                    $lokasi_presensi = $rekap['lokasi_presensi'];
+                    $lokasi = mysqli_query($connection, "SELECT * FROM lokasi_presensi WHERE nama_lokasi = '$lokasi_presensi'");
 
-while ($lokasi_result = mysqli_fetch_array($lokasi)) :
-    $jam_masuk_kantor = date('H:i:s', strtotime($lokasi_result['jam_masuk']));
-endwhile;
-        $jam_masuk = date('H:i:s', strtotime($rekap['jam_masuk']));
-        $timestamp_jam_masuk_real = strtotime($jam_masuk);
-        $timestamp_jam_masuk_kantor = strtotime($jam_masuk_kantor);
-        $terlambat = $timestamp_jam_masuk_real - $timestamp_jam_masuk_kantor;
+                    while ($lokasi_result = mysqli_fetch_array($lokasi)) :
+                        $jam_masuk_kantor = date('H:i:s', strtotime($lokasi_result['jam_masuk']));
+                    endwhile;
+                    $jam_masuk = date('H:i:s', strtotime($rekap['jam_masuk']));
+                    $timestamp_jam_masuk_real = strtotime($jam_masuk);
+                    $timestamp_jam_masuk_kantor = strtotime($jam_masuk_kantor);
+                    $terlambat = $timestamp_jam_masuk_real - $timestamp_jam_masuk_kantor;
 
-        // Set flag untuk status keterlambatan
-        $is_ontime = $terlambat <= 0;
+                    // Set flag untuk status keterlambatan
+                    $is_ontime = $terlambat <= 0;
 
-        if ($terlambat > 0) {
-            $total_jam_terlambat = floor($terlambat / 3600);
-            $terlambat -= $total_jam_terlambat * 3600;
-            $selisih_menit_terlambat = floor($terlambat / 60);
-        } else {
-            $total_jam_terlambat = 0;
-            $selisih_menit_terlambat = 0;
-        }
-    ?>
+                    if ($terlambat > 0) {
+                        $total_jam_terlambat = floor($terlambat / 3600);
+                        $terlambat -= $total_jam_terlambat * 3600;
+                        $selisih_menit_terlambat = floor($terlambat / 60);
+                    } else {
+                        $total_jam_terlambat = 0;
+                        $selisih_menit_terlambat = 0;
+                    }
+                ?>
 
-        <tr>
-            <td> <?= $no++ ?></td>
-            <td><?= $rekap['nama'] ?></td>
-            <td> <?= date('d F Y', strtotime($rekap['tanggal_masuk']))  ?></td>
-            <td class="text-center"> <?= $rekap['jam_masuk'] ?></td>
-            <td class="text-center"> <?= $rekap['jam_keluar'] ?></td>
-            <td class="text-center">
-                <?php if ($rekap['tanggal_keluar'] == '0000-00-00'): ?>
-                    <span>0 Jam 0 Menit</span>
-                <?php else: ?>
-                    <?= $total_jam_kerja . ' Jam ' . $selisih_menit_kerja . ' Menit' ?>
-                <?php endif; ?>
-            </td>
-            <td class="text-center">
-                <?php if ($is_ontime): ?>
-                    <span class="badge bg-success">On Time</span>
-                <?php else: ?>
-                    <?= $total_jam_terlambat . ' Jam ' . $selisih_menit_terlambat . ' Menit' ?>
-                <?php endif; ?>
-            </td>
-        </tr>
-    <?php endwhile; ?>
-<?php } ?>
-</table>
-</div>
+                    <tr>
+                        <td> <?= $no++ ?></td>
+                        <td><?= $rekap['nama'] ?></td>
+                        <td> <?= date('d F Y', strtotime($rekap['tanggal_masuk']))  ?></td>
+                        <td class="text-center"> <?= $rekap['jam_masuk'] ?></td>
+                        <td class="text-center"> <?= $rekap['jam_keluar'] ?></td>
+                        <td class="text-center">
+                            <?php if ($rekap['tanggal_keluar'] == '0000-00-00'): ?>
+                                <span>0 Jam 0 Menit</span>
+                            <?php else: ?>
+                                <?= $total_jam_kerja . ' Jam ' . $selisih_menit_kerja . ' Menit' ?>
+                            <?php endif; ?>
+                        </td>
+                        <td class="text-center">
+                            <?php if ($is_ontime): ?>
+                                <span class="badge bg-success">On Time</span>
+                            <?php else: ?>
+                                <?= $total_jam_terlambat . ' Jam ' . $selisih_menit_terlambat . ' Menit' ?>
+                            <?php endif; ?>
+                        </td>
+                    </tr>
+                <?php endwhile; ?>
+            <?php } ?>
+        </table>
+    </div>
 </div>
 <div class="modal" id="exampleModal" tabindex="-1">
     <div class="modal-dialog" role="document">
